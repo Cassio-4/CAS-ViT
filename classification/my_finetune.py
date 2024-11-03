b6cebdbb402c6cac2ef4ab319fb02c5c122a173d
Cassio-4 <cassiobertasinascimento@gmail.com>
2024-11-03 03:47:07 -0300

    remove target_names (classification_report)

--- classification/my_finetune.py
+++ classification/my_finetune.py
@@ -186,5 +186,4 @@ if __name__ == '__main__':
             res  = pred.argmax(dim=1).cpu().tolist()
             lres += res
             ytrue += target
-    target_names = ['Mollymauk', 'Yuru']
-    print(classification_report(ytrue, lres, target_names=target_names))
+    print(classification_report(ytrue, lres))
